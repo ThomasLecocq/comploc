82991a473ab3118674cd5f6df1cc49e04fbdad24
ThomasLecocq <thomas.lecocq@oma.be>
2017-01-04 18:27:28 +0100

    windows make.bat works fine

--- make.bat
+++ make.bat
@@ -1,36 +1,35 @@
 @ECHO OFF
 if "%1" == "datetime" (
-    gcc datetime.f -c
-)
-
+    gcc datetime.f -std=legacy  -c
+    )
 if "%1" == "vzfillin" (
-    gcc vzfillin.f -lm -lgfortran  -o vzfillin
-)
+    gcc vzfillin.f -lm -lgfortran -std=legacy -o vzfillin
+    )
 if "%1" == "deptable" (
-    gcc deptable.f -lm -lgfortran  -o deptable
-)
+    gcc deptable.f -lm -lgfortran -std=legacy -o deptable
+    )
 if "%1" == "getstlist" (
-    gcc getstlist.f -lm -lgfortran  -o getstlist
-)
+    gcc getstlist.f -lm -lgfortran -std=legacy -o getstlist
+    )
 if "%1" == "phase2bed3" (
-    gcc phase2bed3.f datetime.o -lm -lgfortran  -o phase2bed3
-)
+    gcc phase2bed3.f datetime.o -lm -lgfortran -std=legacy -o phase2bed3
+    )
 if "%1" == "listbed3" (
-    gcc listbed3.f datetime.o -lm -lgfortran  -o listbed3
-)
+    gcc listbed3.f datetime.o -lm -lgfortran -std=legacy -o listbed3
+    )
 if "%1" == "comploc" (
-    gcc comploc.f datetime.o -lm -lgfortran  -o comploc
-)
+    gcc comploc.f datetime.o -lm -lgfortran -std=legacy -o comploc
+    )
 
 
 if "%1" == "all" (
-    gcc datetime.f -c
-    gcc vzfillin.f -lm -lgfortran  -o vzfillin
-    gcc deptable.f -lm -lgfortran  -o deptable
-    gcc getstlist.f -lm -lgfortran  -o getstlist
-    gcc phase2bed3.f datetime.o -lm -lgfortran  -o phase2bed3
-    gcc listbed3.f datetime.o -lm -lgfortran  -o listbed3
-    gcc comploc.f datetime.o -lm -lgfortran  -o comploc
+    gcc datetime.f -c -m32
+    gcc vzfillin.f              -lm -lgfortran -std=legacy -m32 -o vzfillin
+    gcc deptable.f              -lm -lgfortran -std=legacy -m32 -o deptable
+    gcc getstlist.f             -lm -lgfortran -std=legacy -m32 -o getstlist
+    gcc phase2bed3.f datetime.o -lm -lgfortran -std=legacy -m32 -o phase2bed3
+    gcc listbed3.f   datetime.o -lm -lgfortran -std=legacy -m32 -o listbed3
+    gcc comploc.f    datetime.o -lm -lgfortran -std=legacy -m32 -o comploc
 )
 
 if "%1" == "clean" (
